d5698d593a0c959eb5830941388ba3a44318088d
ak98neon <ak98nneon@gmail.com>
2020-04-04 23:36:34 +0300

    Up sdk version to 18, added security storage, created second page with button on prev page.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
+
+final storage = new FlutterSecureStorage();
 
 class MyApp extends StatelessWidget {
   @override
@@ -66,8 +69,14 @@ class _MyHomePageState extends State<MyHomePage> {
         minWidth: MediaQuery.of(context).size.width,
         padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
         onPressed: () {
-          print("Buttin clicked");
-          authUser(emailValue, passwordValue);
+          print("Try to login");
+          authUser(emailValue, passwordValue).then((value) {
+            storage.write(key: "jwt", value: value);
+          });
+          if (storage.read(key: "jwt") != null) {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => MainRoute()));
+          }
         },
         child: Text("Login",
             textAlign: TextAlign.center,
@@ -113,24 +122,44 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-Future<http.Response> authUser(String login, String pass) async {
+class MainRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("FMR"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
+
+Future<String> authUser(String username, String pass) async {
   final http.Response response = await http.post(
-    'http://10.0.2.2:9100/login/auth',
+    'http://10.0.2.2:8080/login',
     headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8',
     },
     body: jsonEncode(<String, String>{
-      'login': login,
+      'username': username,
       'password': pass,
     }),
   );
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response,
     // then parse the JSON.
-    return response;
+    var responseBody = jsonDecode(response.body);
+    return responseBody["token"];
   } else {
     // If the server did not return a 200 OK response,
     // then throw an exception.
-    throw Exception('Failed to load album');
+    throw Exception('Failed user login');
   }
 }
